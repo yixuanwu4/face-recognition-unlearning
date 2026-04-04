07761d18c87a6a6a4fca69b2b88888d1b316fe74
Yixuan <y+github@ixuan.ch>
2026-04-04 20:31:59 +0200

    fix: indentation bug in model

--- model/model.py
+++ model/model.py
@@ -9,14 +9,14 @@ class FaceModel(nn.Module):
     for p in self.backbone.parameters():
       p.requires_grad = False
 
-      for p in self.backbone.features[-3:].parameters():
-        p.requires_grad = True
+    for p in self.backbone.features[-3:].parameters():
+      p.requires_grad = True
 
-      in_features = self.backbone.classifier[1].in_features
-      self.backbone.classifier = nn.Sequential(
-        nn.Dropout(0.2),
-        nn.Linear(in_features, num_classes)
-      )
+    in_features = self.backbone.classifier[1].in_features
+    self.backbone.classifier = nn.Sequential(
+      nn.Dropout(0.2),
+      nn.Linear(in_features, num_classes)
+    )
   
   def forward(self, x):
     return self.backbone(x)
